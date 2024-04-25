0c32fbc03726c939d504fc91455fd00a8e8a9927
Aman Jamwal <aman.jamwal95@gmail.com>
2024-04-25 15:44:26 +1200

    Add configuration for About page

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,6 +25,7 @@ const BlogIndex = ({ data, location }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <Bio />
+      <Link to='/about'>About</Link>
       <ol style={{ listStyle: `none` }}>
         {posts.map(post => {
           const title = post.frontmatter.title || post.fields.slug
