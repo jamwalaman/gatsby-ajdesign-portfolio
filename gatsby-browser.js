0e0cad7cc9d99dd3e7aed1ad01556bfd81c8c492
Aman Jamwal <aman.jamwal95@gmail.com>
2024-04-29 11:13:19 +1200

    Add package for Inter font

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,5 @@
 // custom typefaces
-import "@fontsource-variable/montserrat"
-import "@fontsource/merriweather"
+import "@fontsource/inter"
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -21,7 +21,7 @@ const Layout = ({ location, title, children }) => {
   }
 
   return (
-    <div className="global-wrapper" data-is-root-path={isRootPath}>
+    <div>
       <header className="global-header">{header}</header>
       <main>
         <Link to='/portfolio'>Portfolio</Link>
