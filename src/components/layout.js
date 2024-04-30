d29674b8d860e21e489fba5b2fc71667beaae900
Aman Jamwal <aman.jamwal95@gmail.com>
2024-04-30 21:11:18 +1200

    Add svg logo in header nav

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,45 +1,35 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
+import SvgComponent from "./ajd-logo"
 
-const Layout = ({ location, title, children }) => {
-  const rootPath = `${__PATH_PREFIX__}/`
-  const isRootPath = location.pathname === rootPath
-  let header
-
-  if (isRootPath) {
-    header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
-    )
-  }
-
+const Layout = ({ children }) => {
   return (
     <div>
-      <header className="global-header">{header}</header>
       <main>
-        <Navbar expand="lg">
+        <Navbar expand='lg' className='ajd-nav-header'>
           <Container>
-            <Link to='/' className='navbar-brand'>Aj desgin</Link>
+            <Link to='/' className='navbar-brand'>
+              <SvgComponent />              
+            </Link>
             <Navbar.Toggle aria-controls='basic-navbar-nav' />
             <Navbar.Collapse id='basic-navbar-nav'>
-              <Nav className="me-auto">
-                <Link to='/portfolio' className='nav-link'>Portfolio</Link>
-                <Link to='/about' className='nav-link'>About</Link>
+              <Nav>
+                <Link to='/portfolio' className='nav-link'>portfolio</Link>
+                <Link to='/about' className='nav-link'>about</Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
         </Navbar>
-        {children}
+        <Container>
+          <Row className='justify-content-md-center'>
+            <Col md={9}>{children}</Col>
+          </Row>
+        </Container>
       </main>
       <footer>
         © {new Date().getFullYear()}, Built with
